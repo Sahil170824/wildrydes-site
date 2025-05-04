a75308f9e975a4707c5736fdfdf8834f9a146b78
Sahil170824 <sahil.waykole@utdallas.edu>
2025-05-04 13:59:07 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_CiFYTFAxF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '53cl63em9q4uo8rd2fsm280f9b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'v581884vojn53lrecm0q8a688', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
